b185003bacf91bac840a8e16faaf65bb1f5180f5
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-02-23 00:07:47 +0530

    Added: welcome message

---
+++ src/main/java/com/bridgelabz/jdbc1/Day27_Emp_File_IO/EmployeePayrollService.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.jdbc1.Day27_Emp_File_IO;
+
+public class EmployeePayrollService {
+	public static void main(String[] args) {
+		System.out.println("welcome to File IO");
+	}
+}
---
+++ src/test/java/com/bridgelabz/jdbc1/Day27_Emp_File_IO/AppTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.jdbc1.Day27_Emp_File_IO;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
