6758159384ca52aa2bdd39559befa4921ea40049
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-02-23 01:45:17 +0530

    Added: Creating file for Storing the employee payroll Service entries

--- src/main/java/com/bridgelabz/jdbc1/Day27_Emp_File_IO/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/jdbc1/Day27_Emp_File_IO/EmployeePayrollService.java
@@ -1,51 +1,24 @@
 package com.bridgelabz.jdbc1.Day27_Emp_File_IO;
 
 import java.io.IOException;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.nio.file.WatchService;
 import java.util.ArrayList;
-import java.util.List;
-import java.nio.file.WatchEvent;
-import java.nio.file.WatchKey;
-import static java.nio.file.StandardWatchEventKinds.*;
 
 public class EmployeePayrollService {
 	public void listFilesDirectory() throws IOException, InterruptedException {
-		// Activate watch Service
-		WatchService watchService = FileSystems.getDefault().newWatchService();
+		ArrayList<String> writing = new ArrayList<String>();
 
-		// Convert location to file path
-		Path path = Paths.get("D:\\BridgelabzClassWork\\Assignment-243\\Day27-Emp-File-IO\\src\\main\\java\\com\\bridgelabz\\jdbc1\\Day27_Emp_File_IO");
-
-		// Register watch service on above path ==> Pass events to be watched
-		// (Create/Delete/Modify)
-		path.register(watchService, ENTRY_CREATE, ENTRY_DELETE);
-
-		List<String> logs = new ArrayList<String>();
-
-		boolean poll = true;
-		while (poll) {
-
-			// Reference / Pointer current file or folder watching
-			WatchKey key = watchService.take();
-
-			// Printing the events (Create / Modify / Delete)
-			for (WatchEvent<?> event : key.pollEvents()) {
-				System.out.println("Event kind : " + event.kind() + " - File : " + event.context());
-
-				logs.add("Event kind : " + event.kind() + " - File : " + event.context());
-			}
-
-			Files.write(Paths.get("D:\\BridgelabzClassWork\\Assignment-243\\Day27-Emp-File-IO\\src\\main\\java\\com\\bridgelabz\\jdbc1\\Day27_Emp_File_IO"),
-					logs, StandardOpenOption.CREATE);
-
-			// Reseting the reference so that it can be used later
-			poll = key.reset();
-		}
+		writing.add("Id : 1001");
+		writing.add("Name : Nadeem Akhtar");
+		writing.add("Salary : 84761");
+		writing.add("Date : 23-02-2023");
+		// File Exist => Override
+		// Will create new file if not exist
+		Files.write(Paths.get("D:\\BridgelabzClassWork\\Assignment-243\\Day27-Emp-File-IO\\src\\main\\java\\com\\bridgelabz\\jdbc1\\Day27_Emp_File_IO\\testdemo.txt"),
+				writing, StandardOpenOption.CREATE);
+		System.out.println("Exporting Into File");
 	}
 
 	public static void main(String[] args) throws IOException, InterruptedException {
