01857285447cca78e3f64b7414f7ca6b71cc2973
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-02-23 01:36:44 +0530

    Added: Creating Watch Service program

--- src/main/java/com/bridgelabz/jdbc1/Day27_Emp_File_IO/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/jdbc1/Day27_Emp_File_IO/EmployeePayrollService.java
@@ -1,23 +1,55 @@
 package com.bridgelabz.jdbc1.Day27_Emp_File_IO;
 
 import java.io.IOException;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.WatchService;
+import java.util.ArrayList;
+import java.util.List;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import static java.nio.file.StandardWatchEventKinds.*;
 
 public class EmployeePayrollService {
-	public void empPayrollRead() throws IOException {
-		Path playPath = Paths.get("D:\\BridgelabzClassWork\\Assignment-243\\Day27-Emp-File-IO\\src\\main\\java\\com\\bridgelabz\\jdbc1\\Day27_Emp_File_IO\\");
+	public void listFilesDirectory() throws IOException, InterruptedException {
+		// Activate watch Service
+		WatchService watchService = FileSystems.getDefault().newWatchService();
 
-		Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::print);
-		Files.newDirectoryStream(playPath).forEach(System.out::println);
-		Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp"));
+		// Convert location to file path
+		Path path = Paths.get("D:\\BridgelabzClassWork\\Assignment-243\\Day27-Emp-File-IO\\src\\main\\java\\com\\bridgelabz\\jdbc1\\Day27_Emp_File_IO");
 
+		// Register watch service on above path ==> Pass events to be watched
+		// (Create/Delete/Modify)
+		path.register(watchService, ENTRY_CREATE, ENTRY_DELETE);
+
+		List<String> logs = new ArrayList<String>();
+
+		boolean poll = true;
+		while (poll) {
+
+			// Reference / Pointer current file or folder watching
+			WatchKey key = watchService.take();
+
+			// Printing the events (Create / Modify / Delete)
+			for (WatchEvent<?> event : key.pollEvents()) {
+				System.out.println("Event kind : " + event.kind() + " - File : " + event.context());
+
+				logs.add("Event kind : " + event.kind() + " - File : " + event.context());
+			}
+
+			Files.write(Paths.get("D:\\BridgelabzClassWork\\Assignment-243\\Day27-Emp-File-IO\\src\\main\\java\\com\\bridgelabz\\jdbc1\\Day27_Emp_File_IO"),
+					logs, StandardOpenOption.CREATE);
+
+			// Reseting the reference so that it can be used later
+			poll = key.reset();
+		}
 	}
 
-	public static void main(String[] args) throws IOException {
-		EmployeePayrollService emp = new EmployeePayrollService();
-
-		emp.empPayrollRead();
+	public static void main(String[] args) throws IOException, InterruptedException {
+		EmployeePayrollService obj = new EmployeePayrollService();
+		obj.listFilesDirectory();
 	}
 }
