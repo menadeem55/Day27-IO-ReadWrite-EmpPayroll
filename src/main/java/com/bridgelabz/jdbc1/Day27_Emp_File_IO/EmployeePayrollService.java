0c7927571a4f3567970f2d2eaa3bc3e9537d8fd0
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-02-23 01:51:57 +0530

    Added:  Ability to print number of lines in testdemo.txt

--- src/main/java/com/bridgelabz/jdbc1/Day27_Emp_File_IO/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/jdbc1/Day27_Emp_File_IO/EmployeePayrollService.java
@@ -1,22 +1,23 @@
 package com.bridgelabz.jdbc1.Day27_Emp_File_IO;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
 
 public class EmployeePayrollService {
-	public void listFilesDirectory() throws IOException, InterruptedException {
+	public long listFilesDirectory() throws IOException, InterruptedException {
 
-		List<String> line = Files.readAllLines(Paths.get(
-				"D:\\BridgelabzClassWork\\Assignment-243\\Day27-Emp-File-IO\\src\\main\\java\\com\\bridgelabz\\jdbc1\\Day27_Emp_File_IO\\testdemo.txt"));
-		for (String lines : line) {
-			System.out.println(lines);
-		}
+		long enteries = 0;
+		enteries = Files.lines(new File(
+				"D:\\BridgelabzClassWork\\Assignment-243\\Day27-Emp-File-IO\\src\\main\\java\\com\\bridgelabz\\jdbc1\\Day27_Emp_File_IO\\testdemo.txt")
+				.toPath()).count();
+		System.out.println(enteries);
+		return enteries;
 	}
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		EmployeePayrollService obj = new EmployeePayrollService();
-		obj.listFilesDirectory();
+		EmployeePayrollService emp = new EmployeePayrollService();
+		emp.listFilesDirectory();
+
 	}
 }
