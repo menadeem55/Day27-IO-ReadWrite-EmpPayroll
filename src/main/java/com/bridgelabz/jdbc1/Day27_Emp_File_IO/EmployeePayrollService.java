e006ea1cb9def4c1eaa095b1c844c696968d2daf
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-02-23 00:23:20 +0530

    Added: program to demonstrate file

--- src/main/java/com/bridgelabz/jdbc1/Day27_Emp_File_IO/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/jdbc1/Day27_Emp_File_IO/EmployeePayrollService.java
@@ -1,41 +1,23 @@
 package com.bridgelabz.jdbc1.Day27_Emp_File_IO;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class EmployeePayrollService {
 	public void empPayrollRead() throws IOException {
-		List<String> lines = Files.readAllLines(Paths.get("D:\\BridgelabzClassWork\\Assignment-243\\Day27-Emp-File-IO\\src\\main\\java\\com\\bridgelabz\\jdbc1\\Day27_Emp_File_IO\\readWrite.txt"));
+		Path playPath = Paths.get("D:\\BridgelabzClassWork\\Assignment-243\\Day27-Emp-File-IO\\src\\main\\java\\com\\bridgelabz\\jdbc1\\Day27_Emp_File_IO\\");
 
-		for (String line : lines) {
-
-		}
-
-	}
-
-	public void writeDataEmployeePayRoll() throws IOException {
-		ArrayList<String> data = new ArrayList<>();
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter ID");
-		data.add(sc.next());
-		System.out.println("Enter Name");
-		data.add(sc.next());
-		System.out.println("Enter Salary");
-		data.add(sc.next());
-		Files.write(Paths.get("D:\\BridgelabzClassWork\\Assignment-243\\Day27-Emp-File-IO\\src\\main\\java\\com\\bridgelabz\\jdbc1\\Day27_Emp_File_IO\\readWrite.txt"),
-				data, StandardOpenOption.CREATE);
+		Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::print);
+		Files.newDirectoryStream(playPath).forEach(System.out::println);
+		Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp"));
 
 	}
 
 	public static void main(String[] args) throws IOException {
 		EmployeePayrollService emp = new EmployeePayrollService();
-		emp.writeDataEmployeePayRoll();
+
 		emp.empPayrollRead();
 	}
 }
