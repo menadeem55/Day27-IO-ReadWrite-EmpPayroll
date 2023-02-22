1c21d5e9bf6e2c86cabd7ec9ff3506ce52b8375e
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-02-23 01:48:27 +0530

    Added: Ability to print files from file

--- src/main/java/com/bridgelabz/jdbc1/Day27_Emp_File_IO/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/jdbc1/Day27_Emp_File_IO/EmployeePayrollService.java
@@ -3,22 +3,16 @@ package com.bridgelabz.jdbc1.Day27_Emp_File_IO;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
+import java.util.List;
 
 public class EmployeePayrollService {
 	public void listFilesDirectory() throws IOException, InterruptedException {
-		ArrayList<String> writing = new ArrayList<String>();
 
-		writing.add("Id : 1001");
-		writing.add("Name : Nadeem Akhtar");
-		writing.add("Salary : 84761");
-		writing.add("Date : 23-02-2023");
-		// File Exist => Override
-		// Will create new file if not exist
-		Files.write(Paths.get("D:\\BridgelabzClassWork\\Assignment-243\\Day27-Emp-File-IO\\src\\main\\java\\com\\bridgelabz\\jdbc1\\Day27_Emp_File_IO\\testdemo.txt"),
-				writing, StandardOpenOption.CREATE);
-		System.out.println("Exporting Into File");
+		List<String> line = Files.readAllLines(Paths.get(
+				"D:\\BridgelabzClassWork\\Assignment-243\\Day27-Emp-File-IO\\src\\main\\java\\com\\bridgelabz\\jdbc1\\Day27_Emp_File_IO\\testdemo.txt"));
+		for (String lines : line) {
+			System.out.println(lines);
+		}
 	}
 
 	public static void main(String[] args) throws IOException, InterruptedException {
